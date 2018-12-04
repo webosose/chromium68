be6887a2adcf17be8012f3bff1e8c0f949ca0aac
Julie Jeongeun Kim <jkim@igalia.com>
2018-12-04 21:14:42 +0900

    fixup! [BACKPORT] Add implementation for GetRestoredBounds for DesktopWindowTreeHostPlatform
    It's for ozone_platform_wayland_external.

--- src/ozone/platform/ozone_wayland_window.h
+++ src/ozone/platform/ozone_wayland_window.h
@@ -87,6 +87,9 @@ class OzoneWaylandWindow : public PlatformWindow,
   void MoveCursorTo(const gfx::Point& location) override;
   void ConfineCursorToBounds(const gfx::Rect& bounds) override;
   PlatformImeController* GetPlatformImeController() override;
+  void SetRestoredBoundsInPixels(const gfx::Rect& bounds) override;
+  gfx::Rect GetRestoredBoundsInPixels() const override;
+
   void SetWindowProperty(const std::string& name,
                          const std::string& value) override;
   void ResetCustomCursor() override;
