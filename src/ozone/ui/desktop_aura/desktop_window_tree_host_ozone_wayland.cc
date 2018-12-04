72f5766688adf35452f01d97707ae4cba5ba2144
Julie Jeongeun Kim <jkim@igalia.com>
2018-12-04 21:12:55 +0900

    fixup! [BACKPORT] Cleanup ui::PlatformWindowDelegate interface.
    It's for ozone_platform_wayland_external.

--- src/ozone/ui/desktop_aura/desktop_window_tree_host_ozone_wayland.cc
+++ src/ozone/ui/desktop_aura/desktop_window_tree_host_ozone_wayland.cc
@@ -872,9 +872,6 @@ void DesktopWindowTreeHostOzone::OnDamageRect(const gfx::Rect& damaged_rect) {
   compositor()->ScheduleRedrawRect(damaged_rect);
 }
 
-void DesktopWindowTreeHostOzone::OnAcceleratedWidgetDestroying() {
-}
-
 void DesktopWindowTreeHostOzone::OnAcceleratedWidgetDestroyed() {
   gfx::AcceleratedWidget window = compositor()->ReleaseAcceleratedWidget();
   DCHECK_EQ(window, window_);
@@ -946,8 +943,7 @@ void DesktopWindowTreeHostOzone::OnLostCapture() {
 }
 
 void DesktopWindowTreeHostOzone::OnAcceleratedWidgetAvailable(
-      gfx::AcceleratedWidget widget,
-       float device_pixel_ratio) {
+    gfx::AcceleratedWidget widget) {
   window_ = widget;
   CreateCompositor();
   WindowTreeHost::OnAcceleratedWidgetAvailable();
