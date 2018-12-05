032457d4ee47c9238b236664d2d86cd259be404d
Jose Dapena Paz <jose.dapena@lge.com>
2018-12-06 00:07:31 +0100

    [open][neva_upstreamable][media] Fix video hole computation
    :Release Notes:
    Fix video hole computation with additional contents scale.
    
    :Detailed Notes:
    The additional contents scale computation was completely broken. It was
    applied inconsistently, and giving wrong values.
    
    Modified to only apply the additional contents scale to the end of
    the algorithm, for adapting the visible rect to the screen rect scale.
    
    :Testing Performed:
    Viewster video playback works now. Task switching between viewster and
    photo/video playing video works. Browser video playback with scrolling
    works.
    
    :QA Notes:
    Test-cases: GT-15269, GT-19638, GT-24241
    
    :Issues Addressed:
    [PLAT-69164] [chromium 68] Viewster CP app (incorrect video size)
    
    Change-Id: Ie74e33956045d61bdb0a1ab9f88f36ac32ab7c52
    Originally-Reviewed-on: http://gpro.lgsvl.com/224468
    Reviewed-on: http://gpro.lgsvl.com/224494
    Reviewed-by: OSE Helper Bot <ose@ci.lgsvl.net>
    Reviewed-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
    Tested-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>

--- src/media/base/neva/video_util_neva.cc
+++ src/media/base/neva/video_util_neva.cc
@@ -31,45 +31,44 @@ bool ComputeVideoHoleDisplayRect(const gfx::Rect& video_rect_in_view_space,
   gfx::Rect prev_visible_rect = visible_rect;
   bool prev_is_fullscreen = is_fullscreen;
 
-  // Step2: Compute video_rect. This variable is essential to compute
-  //        source_rect and visible_rect.
-  gfx::Rect video_rect = ScaleToEnclosingRectSafe(
-      video_rect_in_view_space, additional_scale.x(), additional_scale.y());
-
-  // Step3: Compute source_rect.
+  // Step2: Compute source_rect.
   gfx::Rect view_rect_from_origin(0, 0, view_rect.width(), view_rect.height());
-  visible_rect = gfx::IntersectRects(video_rect, view_rect_from_origin);
+  visible_rect =
+      gfx::IntersectRects(video_rect_in_view_space, view_rect_from_origin);
 
   // We check video_rect is partially visible or not. If video_rect is partially
   // visible, source_rect will be part of natural video's rect. Otherwise, it
   // will be identical with natural video's rect.
   gfx::Rect natural_video_rect(0, 0, natural_video_size.width(),
                                natural_video_size.height());
-  if (visible_rect == video_rect || visible_rect.IsEmpty()) {
+  if (visible_rect == video_rect_in_view_space || visible_rect.IsEmpty()) {
     source_rect = natural_video_rect;
   } else {
-    DCHECK(video_rect.width() != 0 && video_rect.height() != 0);
+    DCHECK(video_rect_in_view_space.width() != 0 &&
+           video_rect_in_view_space.height() != 0);
 
-    int source_x = visible_rect.x() - video_rect.x();
-    int source_y = visible_rect.y() - video_rect.y();
+    int source_x = visible_rect.x() - video_rect_in_view_space.x();
+    int source_y = visible_rect.y() - video_rect_in_view_space.y();
 
     // TODO(neva): Make sure when video scales are different between natural
     // video and display rect. Currently we try to fully fit into display rect.
-    float scale_width =
-        static_cast<float>(natural_video_rect.width()) / video_rect.width();
-    float scale_height =
-        static_cast<float>(natural_video_rect.height()) / video_rect.height();
+    float scale_width = static_cast<float>(natural_video_rect.width()) /
+                        video_rect_in_view_space.width();
+    float scale_height = static_cast<float>(natural_video_rect.height()) /
+                         video_rect_in_view_space.height();
 
     source_rect = gfx::Rect(source_x, source_y, visible_rect.width(),
                             visible_rect.height());
     source_rect =
         ScaleToEnclosingRectSafe(source_rect, scale_width, scale_height);
-    source_rect = ScaleToEnclosingRectSafe(source_rect, additional_scale.x(),
-                                           additional_scale.y());
   }
 
-  // Step4: Adjust visible_rect to screen space.
+  // Step3: Adjust visible_rect to view offset.
   visible_rect.Offset(view_rect.x(), view_rect.y());
+
+  // Step4: Adjust visible_rect to screen space.
+  visible_rect = ScaleToEnclosingRectSafe(visible_rect, additional_scale.x(),
+                                          additional_scale.y());
 
   // Step5: Determine is_fullscreen.
   is_fullscreen =
