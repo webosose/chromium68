8a4fc1d95994a32dbf5f92f64c15065eebc50617
Eugene Cherkasov <eugene.cherkasov@lge.com>
2019-01-17 15:26:35 +0300

    [open][neva_upstreamable][input] Fix crash in WaylandTextInput::SetInputContentType
    :Release Notes:
    Fix crash in WaylandTextInput::SetInputContentType
    
    :Detailed Notes:
    Add additional check in WaylandTextInput::SetInputContentType to avoid crash.
    First parameter of text_model_set_content_type can't be null.
    
    :Testing Performed:
    1. Launch pooq application with
      luna-send -n 1 -f luna://com.webos.applicationManager/launch '{"id":"pooq"}'
    2. Press NRC green button
    Verify that application goes to search page and there is no crash
    
    :QA Notes:
    Test-cases: GT-22330, GT-17344
    
    :Issues Addressed:
    [PLAT-71238] [chromium68]Unable to enter values in pooq search field
                 using VKB
    [PLAT-72251] Crash of WebAppMgr when launch quick setting on VKB
    
    Change-Id: I9d6fd76c6ad649c799216299f1294677f67d5955
    Originally-Reviewed-on: http://gpro.lgsvl.com/228250
    Reviewed-on: http://gpro.lgsvl.com/228383
    Reviewed-by: OSE Helper Bot <ose@ci.lgsvl.net>
    Reviewed-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>
    Tested-by: Lokesh Kumar Goel <lokeshkumar.goel@lge.com>

--- src/ozone/wayland/input/webos_text_input.cc
+++ src/ozone/wayland/input/webos_text_input.cc
@@ -207,14 +207,14 @@ void WaylandTextInput::SetHiddenState() {
 void WaylandTextInput::SetInputContentType(ui::InputContentType content_type,
                                            int text_input_flags,
                                            unsigned handle) {
-  if (active_window_ && active_window_->Handle() == handle) {
-    input_content_type_ = content_type;
-    text_input_flags_ = text_input_flags;
+  input_content_type_ = content_type;
+  text_input_flags_ = text_input_flags;
+  if (text_model_ && active_window_ && active_window_->Handle() == handle) {
     if (is_visible_)
-      text_model_set_content_type(text_model_,
-                                  ContentHintFromInputContentType(input_content_type_,
-                                                                  text_input_flags_),
-                                  ContentPurposeFromInputContentType(input_content_type_));
+      text_model_set_content_type(
+          text_model_,
+          ContentHintFromInputContentType(input_content_type_, text_input_flags_),
+          ContentPurposeFromInputContentType(input_content_type_));
   }
 }
 
