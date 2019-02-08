681ee73e1ad86fd0f177dd2553cdeb68983e13be
Nick Diego Yamane <nickdiego@igalia.com>
2019-02-08 07:39:12 +0000

    [BACKPORT] ozone/wayland: Remove WaylandXkbKeyboardLayoutEngine
    Ozone/wayland keyboard input implementation uses libxkbcommon
    to properly handle different keyboard layouts. More specifically,
    it has been using a specialization of XkbKeyboardLayoutEngine,
    whose main functionalities are:
    
      (1) Feed libxkbcommon with modifier events/masks (e.g: shift, ctrl, etc),
        so that it can properly convert keys codes into key symbols later on. [1]
    
      (2) Keep track of modifiers' state using an EventModifiers variable owned
        by WaylandKeyboard singleton.
    
    Even though this approach works pretty well, the Wayland specialization
    of XkbKeyboardLayoutEngine has a weird API and it is actually not needed.
    In this patch, WaylandKeyboard is cleaned up so that WaylandXkbKeyboardLayoutEngine
    can be removed, and functionalities mentioned above are moved to XkbKeyboardLayoutEngine,
    and adpted to (re)use some of its internal structures, thus reducing code replication.
    Additionally, this CL modifies the way WaylandKeyboard interacts with KeyboardLayoutEngine,
    injecting it via its constructor, instead of directly accessing it through
    KeyboardLayoutEngineManager, what is more aligned with KeyboardEvdev implementation
    and general chromium practices.
    
    [1] https://xkbcommon.org/doc/current/group__state.html#ga566677517a286527e05efc5680adbe6b
    
    Bug: 921947
    Change-Id: I5c294e61711d0bf55db95b0e57d1466d00464706
    Reviewed-on: https://chromium-review.googlesource.com/c/1454308
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630238}

--- src/ui/ozone/platform/wayland/wayland_test.cc
+++ src/ui/ozone/platform/wayland/wayland_test.cc
@@ -9,7 +9,7 @@
 #include "ui/platform_window/platform_window_init_properties.h"
 
 #if BUILDFLAG(USE_XKBCOMMON)
-#include "ui/ozone/platform/wayland/wayland_xkb_keyboard_layout_engine.h"
+#include "ui/events/ozone/layout/xkb/xkb_keyboard_layout_engine.h"
 #else
 #include "ui/events/ozone/layout/stub/stub_keyboard_layout_engine.h"
 #endif
@@ -22,8 +22,7 @@ namespace ui {
 WaylandTest::WaylandTest() {
 #if BUILDFLAG(USE_XKBCOMMON)
   KeyboardLayoutEngineManager::SetKeyboardLayoutEngine(
-      std::make_unique<WaylandXkbKeyboardLayoutEngine>(
-          xkb_evdev_code_converter_));
+      std::make_unique<XkbKeyboardLayoutEngine>(xkb_evdev_code_converter_));
 #else
   KeyboardLayoutEngineManager::SetKeyboardLayoutEngine(
       std::make_unique<StubKeyboardLayoutEngine>());
