59fd151c4d542fe490f003216d7c5f7730c2450a
Maksim Sisov <msisov@igalia.com>
2019-02-12 08:06:03 +0000

    [BACKPORT] [ozone/wayland] Use gbm with in-process-gpu mode
    This CL makes Ozone/Wayland use gbm with and without
    --in-process-gpu flag. Though, whenever gbm is not available and
    the mentioned flag is passed, a SurfaceViewGL will be used
    instead.
    
    By doing so, users can use NativeGpuMemory buffers whenever
    a separate gpu process is spawned or not.
    
    Bug: 928269
    Change-Id: I0db754e2d4d9e3ac93c361b5fcbba99287e4befb
    Reviewed-on: https://chromium-review.googlesource.com/c/1463139
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631145}

--- src/ui/ozone/platform/wayland/ozone_platform_wayland.cc
+++ src/ui/ozone/platform/wayland/ozone_platform_wayland.cc
@@ -129,6 +129,11 @@ class OzonePlatformWayland : public OzonePlatform {
 
   bool IsNativePixmapConfigSupported(gfx::BufferFormat format,
                                      gfx::BufferUsage usage) const override {
+    // If there is no drm render node device available, native pixmaps are not
+    // supported.
+    if (path_finder_.GetDrmRenderNodePath().empty())
+      return false;
+
     if (std::find(supported_buffer_formats_.begin(),
                   supported_buffer_formats_.end(),
                   format) == supported_buffer_formats_.end()) {
@@ -151,11 +156,7 @@ class OzonePlatformWayland : public OzonePlatform {
     if (!connection_->Initialize())
       LOG(FATAL) << "Failed to initialize Wayland platform";
 
-#if defined(WAYLAND_GBM)
-    if (!args.single_process)
-      connector_.reset(new WaylandConnectionConnector(connection_.get()));
-#endif
-
+    connector_.reset(new WaylandConnectionConnector(connection_.get()));
     cursor_factory_.reset(new BitmapCursorFactoryOzone);
     overlay_manager_.reset(new StubOverlayManager);
     input_controller_ = CreateStubInputController();
@@ -164,27 +165,23 @@ class OzonePlatformWayland : public OzonePlatform {
   }
 
   void InitializeGPU(const InitParams& args) override {
-    if (!args.single_process) {
-      proxy_.reset(new WaylandConnectionProxy(nullptr));
+    proxy_.reset(new WaylandConnectionProxy(connection_.get()));
 #if defined(WAYLAND_GBM)
-      DrmRenderNodePathFinder path_finder;
-      const base::FilePath drm_node_path = path_finder.GetDrmRenderNodePath();
-      if (drm_node_path.empty())
-        LOG(FATAL) << "Failed to find drm render node path.";
-
-      DrmRenderNodeHandle handle;
-      if (!handle.Initialize(drm_node_path))
-        LOG(FATAL) << "Failed to initialize drm render node handle.";
-
-      auto gbm = CreateGbmDevice(handle.PassFD().release());
-      if (!gbm)
-        LOG(FATAL) << "Failed to initialize gbm device.";
-
-      proxy_->set_gbm_device(std::move(gbm));
-#endif
+    const base::FilePath drm_node_path = path_finder_.GetDrmRenderNodePath();
+    if (drm_node_path.empty()) {
+      LOG(WARNING) << "Failed to find drm render node path.";
     } else {
-      proxy_.reset(new WaylandConnectionProxy(connection_.get()));
+      DrmRenderNodeHandle handle;
+      if (!handle.Initialize(drm_node_path)) {
+        LOG(WARNING) << "Failed to initialize drm render node handle.";
+      } else {
+        auto gbm = CreateGbmDevice(handle.PassFD().release());
+        if (!gbm)
+          LOG(WARNING) << "Failed to initialize gbm device.";
+        proxy_->set_gbm_device(std::move(gbm));
+      }
     }
+#endif
     gpu_platform_support_.reset(CreateStubGpuPlatformSupport());
     surface_factory_.reset(new WaylandSurfaceFactory(proxy_.get()));
   }
@@ -227,6 +224,10 @@ class OzonePlatformWayland : public OzonePlatform {
   std::unique_ptr<WaylandConnectionConnector> connector_;
 
   std::vector<gfx::BufferFormat> supported_buffer_formats_;
+
+  // This is used both in the gpu and browser processes to find out if a drm
+  // render node is available.
+  DrmRenderNodePathFinder path_finder_;
 
   DISALLOW_COPY_AND_ASSIGN(OzonePlatformWayland);
 };
